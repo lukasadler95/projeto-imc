82d26a33dc6185c0430d3b98f3f7e0545c56536b
Lukas Adler <SEU_EMAIL_DO_GITHUB>
2026-05-09 15:57:49 -0300

    Corrigindo ESLint

--- src/imc.js
+++ src/imc.js
@@ -2,7 +2,6 @@ function calcularIMC(peso, altura) {
     if (peso <= 0 || altura <= 0) {
         throw new Error("Peso e altura devem ser maiores que zero");
     }
-    abc =
     return peso / (altura * altura);
 }
 
